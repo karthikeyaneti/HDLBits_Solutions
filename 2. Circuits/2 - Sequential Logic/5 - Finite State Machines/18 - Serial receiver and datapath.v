b15ed217366d3e27626aa4bed6b692f16c51b93c
karthikeyaneti <karthikneti17@gmail.com>
2025-06-21 15:29:45 +0530

    Finite State Machines

--- 2. Circuits/2 - Sequential Logic/5 - Finite State Machines/18 - Serial receiver and datapath.v
+++ 2. Circuits/2 - Sequential Logic/5 - Finite State Machines/18 - Serial receiver and datapath.v
@@ -2,9 +2,13 @@ module top_module(
     input clk,
     input in,
     input reset,    // Synchronous reset
+    output [7:0] out_byte,
     output done
 ); //
+
+    reg [7:0] out;
     
+    // Use FSM from Fsm_serial
     parameter IDLE = 2'd0, DATA = 2'd1, STOP = 2'd2, DONE = 2'd3;
   	reg [1:0] state, next;
   	reg [3:0] count;
@@ -34,6 +38,17 @@ module top_module(
         end
     end
 
+    // New: Datapath to latch input bits.
+        
+  	always @(posedge clk) begin
+        if(reset) 
+            out <= 8'b0;
+        else if(state == DATA && count >= 4'd1 && count <= 4'd8)
+            out[count-1] <= in;
+    end
+
 	assign done = (state == DONE);
+    assign out_byte = (done) ? out : 8'd0;
+  
 
 endmodule
